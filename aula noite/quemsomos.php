afb6cfc6ce50c19ea176b92d5a61e1f17ad96048
MCruzMartins <112035925+MCruzMartins@users.noreply.github.com>
2022-11-23 21:40:54 -0300

    vcsdfv

---
+++ aula noite/quemsomos.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'read.php';
+require_once 'menu.php';
+?>
+
+<div class="container-fluid">
+    <div class="row text-center">
+            <div class="col-md-4">
+                **colocar imagem**
+            </div>
+            <div class= "col-md-8"> 
+                **colocar  texto**
+            </div>
+    </div>
+    
+    <div class="row text-center">
+        <div class="col-md-12">
+    **nossos valores**
+        </div>
+    </div>
+</div>
