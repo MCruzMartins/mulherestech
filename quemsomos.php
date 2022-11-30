66957cb3fb2f5e0e2e972db04570a2bcff7b9c87
MCruzMartins <112035925+MCruzMartins@users.noreply.github.com>
2022-11-29 21:35:51 -0300

    Add files via upload

---
+++ quemsomos.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'read.php';
+require_once 'menu.php';
+?>
+
+<div class="container-fluid">
+    <div class="row servicos">
+            <div class="col-md-6">
+             <img src="imagens/oficina mecanica.webp">
+            </div>
+
+            <div class= "col-md-6"> 
+                Somos uma empresa especializada em veiculos.
+            </div>
+    </div>
+    
+    <div class="row text-center">
+        <div class="col-md-12">
+<h1>Nossos Valores</h1>
+
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+require_once 'footer.php';?>
